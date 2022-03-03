2ba3b742b7a1e8e6d186224e6df03287b46b0a2f
Karl Girthofer <karlgirthofer@Karls-MBP.localdomain>
2022-03-03 12:11:15 -0500

    updating the index function to pass a json decoded, base64 decoded, body to the APIResponse function to properly handle the way API gateway submits payload the lambda integration

--- 0.7-PHP-Lambda-functions-with-Docker-container-images/src/index.php
+++ 0.7-PHP-Lambda-functions-with-Docker-container-images/src/index.php
@@ -21,7 +21,7 @@
 //hello function
 function index($data)
 {
-    return APIResponse("Hello, ". $data['queryStringParameters']['name']);
+    return APIResponse("Hello, ". json_decode(base64_decode($data['body']), true)['queryStringParameters']['name']);
 }
 
 function APIResponse($body)
